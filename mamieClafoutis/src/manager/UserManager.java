0e8b59861e2446e3dc6d4538b3d24eb74e31af32
momo <mohamednefzi.1985@gmail.com>
2017-10-01 14:55:47 -0400

    first commit mohamed

---
+++ mamieClafoutis/src/manager/UserManager.java
@@ -0,0 +1,31 @@
+package manager;
+
+import entities.User;
+//les attribut Private static String Query
+
+public class UserManager {
+
+	public static User getUser(String login, String pwd) {
+		return null;
+	}
+
+	public boolean verifyUser(String login, String pwd) {
+		return false;
+	}
+
+	public User getAllInformationById(int id) {
+		return null;
+	}
+
+	// a l'inscription on appelle insert user
+	public static boolean Insert(User newUser) {
+		return false;
+	}
+
+	// mis a jour d'un employé type de fonction 
+	//ou retirer de la liste avec un boolean
+	public static boolean Update(User user) {
+		return false;
+	}
+
+}
